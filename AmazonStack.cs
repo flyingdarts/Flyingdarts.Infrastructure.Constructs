4925ba2487d79b501dffbd62a1d4053157e885ea
Mike Pattyn <mike.pattyn1@gmail.com>
2023-05-16 15:18:06 +0200

    Initial commit

---
+++ AmazonStack.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Flyingdarts.Infrastructure;
+
+public class AmazonStack : Stack
+{
+
+    public AmazonStack(Construct scope, IStackProps props, string[] repositories) : base(scope, "Flyingdarts-Stack", props)
+    {
+        new BackendConstruct(this, "Backend", repositories.Where(x=>!x.Contains("Signalling")).ToArray());
+        //new AmplifyConstruct(this, "Frontend");
+        new AuthConstruct(this, "OIDC", repositories);
+    }
+}
