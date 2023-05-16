9a31c6c8641416b7239274a5bc111718c73570d2
Mike Pattyn <mike.pattyn1@gmail.com>
2023-05-16 16:01:57 +0200

    also filter out infrastructure repositories

--- AmazonStack.cs
+++ AmazonStack.cs
@@ -6,7 +6,7 @@ public class AmazonStack : Stack
 
     public AmazonStack(Construct scope, IStackProps props, string[] repositories) : base(scope, "Flyingdarts-Stack", props)
     {
-        new BackendConstruct(this, "Backend", repositories.Where(x=>!x.Contains("Signalling")).ToArray());
+        new BackendConstruct(this, "Backend", repositories.Where(x=>!x.Contains("Signalling") || !x.Contains("Infrastructure")).ToArray());
         //new AmplifyConstruct(this, "Frontend");
         new AuthConstruct(this, "OIDC", repositories);
     }
